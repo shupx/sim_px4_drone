73205ab256293a4c5d8b0b4e45bcd0fda9a12682
Peixuan Shu <shupeixuan@qq.com>
2026-01-08 21:58:02 +0800

    update dep

--- install_dep.sh
+++ install_dep.sh
@@ -8,6 +8,7 @@ sudo apt install ros-noetic-mavros ros-noetic-mavros-extras ros-noetic-robot-sta
 pip3 install PyQt5
 
 # marsim_render
+sudo apt-get install libglfw3-dev libglew-dev libncurses5-dev libncursesw5-dev -y
 
 # local_sensing_sim
 # pip3 install open3d numpy pyyaml # for map_generator.py (optional)
